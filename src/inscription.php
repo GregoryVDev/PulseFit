7db4bb29ef14cf05582dbf7ca63c1dd443bf9ed8
GregoryVDev <arexo@hotmail.fr>
2025-02-07 17:36:11 +0100

    Added input email and input firstname in right-container

--- src/inscription.php
+++ src/inscription.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>PulseFit</title>
 </head>
 
@@ -13,7 +15,16 @@
             <div class="left-container">
                 <img src="./images/sallegym.jpg" alt="Salle de sport">
             </div>
-            <div class="right-container"></div>
+            <div class="right-container">
+                <div class="container-firstname">
+                    <label for="prenom">Prénom :</label>
+                    <input type="text" class="form-input" name="firstname" id="firstname" placeholder="Prénom">
+                </div>
+                <div class="container-email">
+                    <label for="email">E-mail :</label>
+                    <input type="email" class="form-input" name="email" id="email" placeholder="Email">
+                </div>
+            </div>
         </div>
     </main>
 </body>
