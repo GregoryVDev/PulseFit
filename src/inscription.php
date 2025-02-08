785516e18fa9829063a02d63a626e7cdf8b88459
GregoryVDev <arexo@hotmail.fr>
2025-02-08 19:49:30 +0100

    Added the paragraph

--- src/inscription.php
+++ src/inscription.php
@@ -30,6 +30,7 @@
                         <input type="password" class="form-input" name="password" id="password" placeholder="Mot de passe">
                     </div>
                 </form>
+                <p>Vous avez un compte ? cliquez <span class="orange">ici</span></p>
             </div>
         </div>
     </main>
