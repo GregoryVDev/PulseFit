382aa518f194de99bde6b0e7ce6d1126aeb988ad
GregoryVDev <arexo@hotmail.fr>
2025-02-10 16:45:07 +0100

    Created navbar.css and added html structure

---
+++ src/template/navbar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <link rel="stylesheet" href="./navbar.css">
+    <title>PulseFit</title>
+</head>
+
+<body>
+
+
+</body>
