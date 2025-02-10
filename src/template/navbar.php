2654fc3d2b136cdf71e9999c579be38f7a444ff6
GregoryVDev <arexo@hotmail.fr>
2025-02-10 16:55:00 +0100

    Added h1, and ul with li balises

--- src/template/navbar.php
+++ src/template/navbar.php
@@ -12,6 +12,28 @@
 </head>
 
 <body>
-
+    <header>
+        <div class="container-left">
+            <div class="logo">
+                <h1>PulseFit</h1>
+            </div>
+        </div>
+        <div id="burger-menu">
+            <span></span>
+        </div>
+        <nav id="nav">
+            <ul class="nav_list">
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="../games.php">Coachs</a></li>
+                <li><a href="../reviews.php">Services</a></li>
+                <li><a href="../connexion.php" class="button-connect">Formulres</a></li>
+                <li><a href="../inscription.php" class="button-inscri">Contact</a></li>
+            </ul>
+            <div class="container-buttons">
+                <a href="../connexion.php" class="button-connection">Connexion</a>
+                <a href="../inscription.php" class="button-inscription">Réserver</a>
+            </div>
+        </nav>
+    </header>
 
 </body>
