a278d047f445e32453b8e45b1b1d07f6e9a3455a
GregoryVDev <arexo@hotmail.fr>
2025-02-11 18:26:45 +0100

    Added html structure

---
+++ src/template/footer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../template/navbar.css">
+    <link rel="stylesheet" href="../template/footer.css">
+    <title>PulseFit</title>
+</head>
+
+<body>
+    <footer>
+        <div class="container-logo">
+            <h1>PulseFit</h1>
+        </div>
+        <div class="container-li">
+            <h3>Menu</h3>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Coachs</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Formules</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="container-support">
+            <h3>Contactez-nous</h3>
+            <p>Adresse : 15 rue du château 75001 Paris, France</p>
+            <p>E-mail : info@pulsefit.fr</p>
+            <p>Tel : 01 23 45 67 89</p>
+        </div>
+        <div class="container-times">
+            <h3>Horaires</h3>
+            <p>Lun - Sam : 6h - 23h</p>
+            <p>Dimanche : 9h - 19h</p>
+            <p>Fermé les jours fériés</p>
+        </div>
+    </footer>
+    <div class="footer-bottom">
+        <p>Mentions légales</p>
+        <p>Politique de confidentialité</p>
+        <p>Condition d'utilisation</p>
+        <p>© 2025 - Grégory</p>
+    </div>
+</body>
+
+</html>
