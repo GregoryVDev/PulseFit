3de003e5a5e9a5ac77add9e3e539d2073c520671
GregoryVDev <arexo@hotmail.fr>
2025-02-17 13:06:31 +0100

    Added h1 on the carousel

--- src/index.php
+++ src/index.php
@@ -1,5 +1,8 @@
 <?php include "./template/navbar.php" ?>
 <main>
+    <div class="overlay">
+        <h1>Repoussez vos limites !</h1>
+    </div>
     <section class="illustration">
         <img src="./images/man.jpg" alt="Homme">
         <img src="./images/woman.jpg" alt="Femme">
