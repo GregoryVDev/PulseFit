ed8fde2f91bd8c1b5b94de407f6dfe78a6b1d6bb
GregoryVDev <arexo@hotmail.fr>
2025-02-17 15:03:44 +0100

    Added article in illustration section and coachs section

--- src/index.php
+++ src/index.php
@@ -4,9 +4,15 @@
         <h1>Repoussez vos limites !</h1>
     </div>
     <section class="illustration">
-        <img src="./images/man.jpg" alt="Homme">
-        <img src="./images/woman.jpg" alt="Femme">
-        <img src="./images/woman2.jpg" alt="Femme">
+        <article class="img">
+            <img src="./images/man.jpg" alt="Homme">
+        </article>
+        <article class="img">
+            <img src="./images/woman.jpg" alt="Femme">
+        </article>
+        <article class="img">
+            <img src="./images/woman2.jpg" alt="Femme">
+        </article>
         <div class="buttons-container">
             <button class="btn-orange"><a href="#">Découvrez les formules</a></button>
             <button class="btn-black"><a href="#">Réserver un cours</a></button>
@@ -14,6 +20,55 @@
     </section>
     <section class="about">
         <img src="./images/dos.jpg" alt="">
+        <div class="container-about">
+            <h2>PulseFit</h2>
+            <p>Nous t'accompagnons dans ta transformation physique et ton bien-être en te fournissant des conseils adaptés, des programmes d'entraînement efficaces et des astuces nutritionnelles.</p>
+            <a href="#">Savoir plus</a>
+        </div>
+    </section>
+    <section class="coachs">
+        <h2>Nos coachs</h2>
+        <article class="coach">
+            <figure>
+                <img src="./images/laura.jpg" alt="Coach Laura">
+                <figcaption>
+                    <h3>Laura</h3>
+                    <p>(CrossFit)</p>
+                    <p>Laura vous accompagne dans des entraînements intenses et variés, alliant force, endurance et explosivité.</p>
+                </figcaption>
+            </figure>
+        </article>
+        <article class="coach">
+            <figure>
+                <img src="./images/maxime.jpg" alt="Coach Maxime">
+                <figcaption>
+                    <h3>Maxime</h3>
+                    <p>(Perte de poids)</p>
+                    <p>Maxime est spécialisé dans l'accompagnement des personnels souhaitant perdre du poids et retrouver leur forme.</p>
+                </figcaption>
+            </figure>
+        </article>
+        <article class="coach">
+            <figure>
+                <img src="./images/amandine.jpg" alt="Coach Laura">
+                <figcaption>
+                    <h3>Amandine</h3>
+                    <p>(Cardio-Training)</p>
+                    <p>Amandine vous aide a améliorer votre endurance et brûler des graisses avec des séances de cardio dynamiques.</p>
+                </figcaption>
+            </figure>
+        </article>
+        <article class="coach">
+            <figure>
+                <img src="./images/pierre.jpg" alt="Coach Laura">
+                <figcaption>
+                    <h3>Pierre</h3>
+                    <p>(Musculation)</p>
+                    <p>Pierre est un expert en musculation et vous aide à développer votre force et votre masse musculaire.</p>
+                </figcaption>
+            </figure>
+        </article>
+
     </section>
 </main>
 <?php include "./template/footer.php" ?>
