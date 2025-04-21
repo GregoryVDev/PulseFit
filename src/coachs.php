e720be1180876426d9de30fefc12ade65f652b5e
GregoryVDev <arexo@hotmail.fr>
2025-04-21 19:01:32 +0200

    Added content for Laura coach

--- src/coachs.php
+++ src/coachs.php
@@ -1,5 +1,20 @@
 <?php include "./template/navbar.php" ?>
 <main>
     <h1>Nos coachs vous accompagne.</h1>
+
+    <article class="card-coachs">
+        <h3>Laura</h3>
+        <h6>(CrossFit)</h6>
+        <p>
+            Passionnée par le sport et le dépassement de soi, Laura est votre coach spécialisée en CrossFit. Avec plusieurs années d'expérience, elle vous accompagne dans des entraînements intenses et variés, alliant force, endurance et explosivité.
+            Que vous soyez débutant ou athlète confirmé, Laura adapte chaque séance à votre niveau pour vous aider à repousser vos limites et atteindre vos objectifs. Son énergie et sa pédagogie font d'elle une coach motivante et à l'écoute, prête à vous challenger dans une ambiance dynamique et conviviale.
+        </p>
+        <p>Objectif : vous rendre plus fort, plus rapide et plus endurant, séance après séance !</p>
+
+    </article>
+    <article class="card-coachs">
+    </article>
+    <article class="card-coachs"></article>
+    <article class="card-coachs"></article>
 </main>
 <?php include "./template/footer.php" ?>
