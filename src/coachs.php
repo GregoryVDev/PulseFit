aa08280fda5282c56105292bc10d0b24ade61a73
GregoryVDev <arexo@hotmail.fr>
2025-04-21 19:05:29 +0200

    Added content for Amandine coach

--- src/coachs.php
+++ src/coachs.php
@@ -21,7 +21,15 @@
         </p>
         <p>Objectif : vous aider à retrouver confiance en vous et à atteindre vos objectifs de perte de poids !</p>
     </article>
-    <article class="card-coachs"></article>
+    <article class="card-coachs">
+        <h3>Amandine</h3>
+        <h6>(Cardio-Training)</h6>
+        <p>
+            Amandine est passionnée par le cardio-training et l'amélioration de l'endurance. Elle vous propose des séances variées qui allient travail cardio-vasculaire, tonification et brûlage de graisses. Elle vous guide à travers des exercices adaptés à votre niveau pour booster votre cœur et votre forme physique.
+            Avec une approche dynamique et stimulante, Amandine vous aide à repousser vos limites tout en prenant soin de votre corps. Sa motivation et son enthousiasme feront de chaque séance un moment agréable pour améliorer votre condition physique.
+        </p>
+        <p>Objectif : renforcer votre cœur, améliorer votre endurance et brûler des calories de manière ludique !</p>
+    </article>
     <article class="card-coachs"></article>
 </main>
 <?php include "./template/footer.php" ?>
